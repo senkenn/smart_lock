29137d2fff0b1150948c10203cb0a92abc35f1cb
senkenn <nayu3898@gmail.com>
2022-09-24 11:19:14 +0900

    Add wifi web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
 
 #define BTM_PIN   15
 #define SERVO_PIN 4
@@ -10,7 +12,29 @@
 #define WAIT_TIME_TO_LOCK 180 * 1000 // ms
 
 Servo      servo;
+WiFiServer server(80);
+
 void setup() {
+
+  // シリアル通信
+  Serial.begin(9600);
+
+  // WiFi
+  WiFi.begin(); // 一度SSIDとパスワードを書き込めばESP32内部に保存される
+  Serial.print("WiFi connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    WiFi.mode(WIFI_STA);
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println(" connected!");
+
+  server.begin();
+
+  Serial.print("HTTP Server: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
 
   // ピン
   pinMode(BTM_PIN, INPUT);
@@ -21,6 +45,12 @@ void setup() {
 }
 
 void loop() {
+
+  // WiFi
+  WiFiClient client = server.available();
+  if (client) {
+    client.println("Hello World!");
+  }
 
   // ボタンの状態を取得
   int state = digitalRead(BTM_PIN);
